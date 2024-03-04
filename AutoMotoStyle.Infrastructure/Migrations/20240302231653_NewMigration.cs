7245b409a72c48f9029e3ad51055d0164774fda3
Tsvetelina Kirilova <cveti.kirilova@gmail.com>
2024-03-04 09:30:52 +0100

    .

---
+++ AutoMotoStyle.Infrastructure/Migrations/20240302231653_NewMigration.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoMotoStyle.Infrastructure.Migrations
+{
+    public partial class NewMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "dealer-7-4421-47c0-a9ba-38b9a5ddb357",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "9275b023-e413-49a0-b381-3a0263ac8414", "AQAAAAEAACcQAAAAEJmjc3VG6pUVdiAC4UQIY/zWP0uV6zItOTC0fhM9bOrcardjdfTY2V3uSf7N6iH6bg==", "4485dbfc-78a5-412d-8965-60aefd03e490" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "guest-a5-7a9a-4ba7-8768-66ba10cd0979",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "35c972d5-4884-46a3-82f4-2a423fab8b97", "AQAAAAEAACcQAAAAEAKiard2Q1IKiyNDX9iqx3k9ZzBVJA5d98TanShN2i+stAyMd63rwzU0Q+0713+lig==", "1edb6dd3-5b6e-4c3b-8f3c-b19ffa5974ff" });
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://www.coches.com/fotos_historicas/audi/A3-Sedan-2.0-TDI-Quattro-S-Line-2016/audi_a3-sedan-2-0-tdi-quattro-s-line-2016_r14.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://cdn.dealeraccelerate.com/pjs/1/456/13143/1920x1440/2007-chevrolet-corvette-z06-procharged.webp");
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://img.indianautosblog.com/resize/750x-/2017/10/2018-Skoda-Superb-facelift-rendering-front-three-quarters.jpg");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "dealer-7-4421-47c0-a9ba-38b9a5ddb357",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "584943d8-0ae3-4116-88bf-3c750196cb26", "AQAAAAEAACcQAAAAEBPiyQm8BqPW611e4Hp3shUpdwoWq43OQbNeP8SZfeVBGc5TfvAMqa5GY1XzMgeAlw==", "a5757806-f298-4a2f-8fb6-e256e17025c0" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "guest-a5-7a9a-4ba7-8768-66ba10cd0979",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "bb7cd6de-d131-47ba-99ab-2672e1eeb476", "AQAAAAEAACcQAAAAENWKgmG0krDzt8B5jhk2r4uItwZcEowj8o5ujxkk869Wc+6p6EnVjjDlGyGVtfBgiw==", "dcb47954-a74a-4496-a329-254cc81dcea0" });
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "");
+        }
+    }
+}
